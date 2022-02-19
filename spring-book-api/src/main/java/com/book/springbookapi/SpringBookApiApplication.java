8b509f89ca61fed9a9ce73242334c6e93966dcd6
Joao Alexandre <aleramosfilho@gmail.com>
2022-02-18 21:51:02 -0300

    starting spring project

---
+++ spring-book-api/src/main/java/com/book/springbookapi/SpringBookApiApplication.java
@@ -0,0 +1,13 @@
+package com.book.springbookapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBookApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBookApiApplication.class, args);
+	}
+
+}
---
+++ spring-book-api/src/test/java/com/book/springbookapi/SpringBookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book.springbookapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
